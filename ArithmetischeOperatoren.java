f594a45e9e8728b6c9e119ef93a13cfb23587268
Lasse <lasse@dornweb.de>
2025-10-07 20:08:13 +0200

    Fehler korrigiert

---
+++ ArithmetischeOperatoren.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class ArithmetischeOperatoren {
+    public static void main(String[] args) {
+    // Scanner-Objekt für die Eingabe
+    Scanner scanner = new Scanner(System.in);
+
+    // Benutzer zur Eingabe von zwei Ganzzahlen auffordern
+    System.out.print("Geben Sie die erste Ganzzahl ein: ");
+    int zahl1 = scanner.nextInt();
+
+    System.out.print("Geben Sie die zweite Ganzzahl ein: ");
+    int zahl2 = scanner.nextInt();
+
+    //Berechnungen durchführen
+    int addition = zahl1 + zahl2;
+    int subtraktion = zahl1 - zahl2;
+    int multiplikation = zahl1 * zahl2;
+
+    //Ergebnisse ausgeben
+    System.out.println("Addition: " + addition);
+    System.out.println("Subtraktion: " + subtraktion);
+    System.out.println("Multiplikation: " + multiplikation);
+    
+    // Überprüfen, ob die Division durch null vermieden wird
+    if (zahl2 !=0) {
+        double division = (double) zahl1 / zahl2; // Division als Gleitkommazahl
+
+        System.out.println("Division: " + division);
+    } else {
+        System.out.println("Die Division durch null ist nicht erlaubt");
+    }
+
+    //Scanner schließen
+    scanner.close();
+    }
+}
